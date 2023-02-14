fecbfc4fe4455d4a82f093ab2baaeedc31279cf4
Kevin <kevinvillacres97@gmail.com>
2023-02-14 16:39:41 +0100

    2 commit

--- main.tf
+++ main.tf
@@ -1,2 +1,26 @@
-wqwq:wq
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami = "ami-0b752bf1df193a6c4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "hello-terraform"
+    APP = "vue2048"
+  }
+}
+
 
