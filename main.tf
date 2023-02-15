181b87ea45de0b8b37e873052df116c7464b045f
Kevin <kevinvillacres97@gmail.com>
2023-02-15 10:34:21 +0100

    Soluciono los bugs

--- main.tf
+++ main.tf
@@ -21,30 +21,24 @@ resource "aws_instance" "app_server" {
   ]
   subnet_id = "subnet-0618f5dbc5a4b2f81"
   key_name  = "clavelucatic2"
+
   tags = {
     Name = var.instance_name
     APP  = "vue2048"
   }
-
-  user_data_base64 = base64encode(<<EOF
-#!/bin/sh
-amazon-linux-extras install -y docker
-
-service docker start
-systemctl enable docker
-
-usermod -a -G docker ec2-user
-
-pip3 install docker-compose
-
-wget https://raw.githubusercontent.com/qebyn/hello-2048/main/docker-compose.yml
-
-chown ec2-user docker-compose.yml
-
-docker-compose pull
-
-docker-compose up -d
-EOF
-  )
+  user_data                   = <<EOH
+  #!/bin/bash
+  amazon-linux-extras install -y docker
+  service docker start
+  usermod -a -G docker ec2-user
+  pip3 install docker-compose
+  mkdir /home/ec2-user/hello2048
+  cd /home/ec2-user/hello2048
+  wget https://raw.githubusercontent.com/qebyn/hello-2048/main/docker-compose.yml
+  docker-compose pull
+  docker-compose up -d
+  chown -R ec2-user:ec2-user /home/ec2-user/hello2048
+  EOH
+  user_data_replace_on_change = true
 }
 
