4c6e7e78bd54da44c08dd4bbb17574661a5587d2
Kevin <kevinvillacres97@gmail.com>
2023-02-16 00:39:01 +0100

    Creo la segunda rama para que este todo mas ordenado

--- variables.tf
+++ variables.tf
@@ -1,11 +1,5 @@
 variable "instance_name" {
-  description = "Value of the Name tag for the EC2 instance"
+  description = "Value of the Name tag for the EC instance"
   type        = string
-  default     = "DefaultName"
-}
-
-variable "app_name" {
-  description = "Value of the App tag for the EC2 instance"
-  type        = string
-  default     = "AppName"
+  default     = "InstanciaTerraform"
 }
