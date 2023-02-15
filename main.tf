e0ca9b4c95e2085afb25ca5c0b30f9ec94b80344
Kevin <kevinvillacres97@gmail.com>
2023-02-15 08:12:13 +0100

    Cambio la clave,subnet,y elgrupo de sg

--- main.tf
+++ main.tf
@@ -16,9 +16,13 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-0b752bf1df193a6c4"
   instance_type = "t2.micro"
-
+  vpc_security_group_ids = [
+    "sg-00f6a6f34d22821b4",
+  ]
+  subnet_id = "subnet-0618f5dbc5a4b2f81"
+  key_name = "clavelucatic2"  
   tags = {
-    Name = "hello-terraform"
+    Name = "Terraforminstancia"
     APP  = "vue2048"
   }
 }
